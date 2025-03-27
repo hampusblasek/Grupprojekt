750a7233afb15f46cc6963b86800d896ebc1efe4
Hampus.Blasek <Hampus_blasek@hotmail.com>
2025-03-27 09:53:17 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+
+namespace Grupprojekt;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.Run();
+    }
+}
