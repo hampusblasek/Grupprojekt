4adfe73f1b160fd7fca34110c66715b81fa68505
CarolinaHoglund <carolinagok1@gmail.com>
2025-04-04 12:27:04 +0200

    Before the big merge

--- obj/Debug/net8.0/Grupprojekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/Grupprojekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grupprojekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6eb15b6205270fd11a0c5798adb91950ea6f060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9494d1123da94ad2ad26cd1027113fb31d049565")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grupprojekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grupprojekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
