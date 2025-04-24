40d9bc778e8e48b2bc4e727d67059c6e3ffad6a0
Hampus.Blasek <Hampus_blasek@hotmail.com>
2025-04-24 08:58:18 +0200

    push after dotnet build

--- obj/Debug/net8.0/Grupprojekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/Grupprojekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grupprojekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4d0f663576fbd7ff7c04be47be284f62569be03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50583b06275d78c27f8013b137449988c6ef5608")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grupprojekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grupprojekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
